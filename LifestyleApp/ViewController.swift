7a6fc182915bf17e40d9af771e10d335bc6ba086
zyy0098 <770823669@qq.com>
2023-12-05 21:50:37 -0500

    Initial Commit

---
+++ LifestyleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifestyleApp
+//
+//  Created by ZYY on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
